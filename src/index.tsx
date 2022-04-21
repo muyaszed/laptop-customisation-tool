0dc6b31df75f44972e99097db4c904e06d6a0db1
Yazed Jamal <yazedjamal@Yazeds-Air.lan>
2022-04-21 13:38:57 +0100

    Addded react and all configurations

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import { Home } from "./pages";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
---
+++ src/test.test.ts
@@ -0,0 +1,7 @@
+describe("test function", () => {
+  it("should return true", () => {
+    expect(15).toBe(15);
+  });
+});
+
+export {};
