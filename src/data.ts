6c8116400fc66f8bbe2c9470d84f068fc29d9491
Yazed Jamal <yazedjamal@Yazeds-Air.lan>
2022-04-22 11:37:55 +0100

    Create mocked data

---
+++ src/data.ts
@@ -0,0 +1,87 @@
+export const data = [
+  {
+    id: "1",
+    title: "Screen",
+    options: [
+      {
+        id: "1",
+        name: '13" Retina',
+        price: "£400",
+      },
+      {
+        id: "2",
+        name: '15" Retina',
+        price: "£700",
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "CPU",
+    options: [
+      {
+        id: "1",
+        name: "2.7GHz quad-core Intel Core i7 processor, Turbo Boost up to 3.6GHz",
+        price: "£300",
+      },
+      {
+        id: "2",
+        name: "2.9GHz quad-core Intel Core i7 processor, Turbo Boost up to 3.8GHz",
+        price: "£500",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "Storage",
+    options: [
+      {
+        id: "1",
+        name: "512GB PCIe-based SSD",
+        price: "£100",
+      },
+      {
+        id: "2",
+        name: "1TB PCIe-based SSD",
+        price: "£200",
+      },
+      {
+        id: "3",
+        name: "2TB PCIe-based SSD",
+        price: "£300",
+      },
+    ],
+  },
+  {
+    id: "4",
+    title: "Memory",
+    options: [
+      {
+        id: "1",
+        name: "8 GB 2133MHz memory",
+        price: "£100",
+      },
+      {
+        id: "2",
+        name: "16 GB 2133MHz memory",
+        price: "£200",
+      },
+    ],
+  },
+  {
+    id: "5",
+    title: "Graphics",
+    options: [
+      {
+        id: "1",
+        name: "Radeon Pro 455 with 2GB memory",
+        price: "£300",
+      },
+      {
+        id: "2",
+        name: "1Radeon Pro 460 with 4GB memory",
+        price: "£400",
+      },
+    ],
+  },
+];
