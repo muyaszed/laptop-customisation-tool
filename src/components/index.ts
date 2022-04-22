0d705acdd71ca81d6b75d9050fe5d12e80d0d847
Yazed Jamal <yazedjamal@Yazeds-Air.lan>
2022-04-22 11:50:35 +0100

    Added exports

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as SpecComponent } from "./SpecComponent/specComponent";
+export { default as Header } from "./Header/header";
+export { default as Content } from "./Content/content";
+export { default as Box } from "./Box/box";
+export { default as OptionItem } from "./OptionItem/optionItem";
+
+export type { IComponentOption } from "./OptionItem/optionItem";
+export type { IBoxProps } from "./Box/box";
